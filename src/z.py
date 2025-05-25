6ce3a5a45d4d4d574d51669728e0dcfd07516e95
Dyno-code-hub <discworldvimes@gmail.com>
2025-05-25 10:59:54 +0100

    setup test

--- src/z.py
+++ src/z.py
@@ -2,3 +2,5 @@ import o
 
 x = 3
 print(o.h)
+def add2_numbers(x,y):
+    return x+y
---
+++ tests/test_z.py
@@ -0,0 +1,4 @@
+from src import z
+def test_add2_numbers():
+    result = z.add2_numbers(3,7)
+    assert result == 10
